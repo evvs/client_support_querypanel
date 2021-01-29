f1dcf15acfb2f909b8c554d5a262db7afff29ea4
evvs <va.evgeny@gmail.com>
2021-01-29 23:20:57 +0300

    added global styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,20 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-const App: React.FC = () => <div className="App">...</div>;
+import LoginPage from './pages/LoginPage';
+import GlobalStyles from './GlobalStyles';
+
+const App: React.FC = () => (
+  <>
+    <div>
+      <Switch>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+      </Switch>
+    </div>
+    <GlobalStyles />
+  </>
+);
+
 export default App;
