85db621ac732cb6f381a3d077997ac93160a53f9
evvs <va.evgeny@gmail.com>
2021-01-31 20:49:06 +0300

    init login page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -8,9 +8,35 @@ const Wrapper = styled.div`
   height: 100vh;
 `;
 
+const LoginWrapper = styled.div`
+  min-height: 70vh;
+  width: 30vw;
+  border: solid 1px white;
+  display: flex;
+  flex-direction: column;
+  header {
+    min-height: 5vh;
+    border: solid 1px red;
+    display: flex;
+    justify-content: space-around;
+    p{
+      padding-bottom: 2px;
+      display: flex;
+      align-items: flex-end;
+      font-size: 2rem;
+      text-decoration: underline;
+    }
+  }
+`;
+
 const LoginPage: React.FC = () => (
   <Wrapper>
-    <div>Hello</div>
+    <LoginWrapper>
+      <header>
+        <p>sign in</p>
+        <p>sign up</p>
+      </header>
+    </LoginWrapper>
   </Wrapper>
 );
 
