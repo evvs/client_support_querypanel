79f0d9bdf31e8c6e7515a6a275028650f8f1f16d
evvs <va.evgeny@gmail.com>
2021-02-04 15:04:21 +0300

    added request func

--- frontend/src/hooks/useHttp.ts
+++ frontend/src/hooks/useHttp.ts
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-type methodsTypes = 'get' | 'post' | 'put' | 'patch' | 'delete'
+type methodsTypes = 'get' | 'post' | 'put' | 'patch' | 'delete';
 
 type useHttpTypes = (
   url: string,
@@ -10,18 +10,28 @@ type useHttpTypes = (
   headers?: Record<string, unknown>,
 ) => void;
 
-export const useHttp: useHttpTypes = async (
+export const useHttp: useHttpTypes = (
   url,
   method = 'get',
   body = null,
   headers = {},
 ) => {
-  const data = await axios({
-    method,
-    url,
-    data: body,
-    headers,
-  });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-  console.log(data);
+  const request = async () => {
+    try {
+      const data = await axios({
+        method,
+        url,
+        data: body,
+        headers,
+      });
+
+      console.log(data);
+    } catch (err) {
+      setError(err);
+      console.log(err);
+    }
+  };
 };
