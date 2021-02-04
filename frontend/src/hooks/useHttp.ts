a1ad903898e70ae7995097994921a60c3da6bbd4
evvs <va.evgeny@gmail.com>
2021-02-04 14:48:56 +0300

    init usehttp hook

--- frontend/src/hooks/useHttp.ts
+++ frontend/src/hooks/useHttp.ts
@@ -1,4 +1,27 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-export const useHttp = () => {};
+type methodsTypes = 'get' | 'post' | 'put' | 'patch' | 'delete'
+
+type useHttpTypes = (
+  url: string,
+  method?: methodsTypes,
+  body?: string | null,
+  headers?: Record<string, unknown>,
+) => void;
+
+export const useHttp: useHttpTypes = async (
+  url,
+  method = 'get',
+  body = null,
+  headers = {},
+) => {
+  const data = await axios({
+    method,
+    url,
+    data: body,
+    headers,
+  });
+
+  console.log(data);
+};
