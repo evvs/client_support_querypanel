84d53ace31e7bed909373dc0fae92c4fdb1b3891
evvs <va.evgeny@gmail.com>
2021-02-18 23:32:09 +0300

    init

---
+++ frontend/cypress/integration/sample.spec.js
@@ -0,0 +1,13 @@
+describe('My First Test', () => {
+  it('Make an assertion', () => {
+    cy.visit('https://example.cypress.io');
+
+    cy.pause();
+
+    cy.contains('type').click();
+
+    cy.url().should('include', '/commands/actions');
+
+    cy.get('.action-email').type('fake@email.com').should('have.value', 'fake@email.com');
+  });
+});
